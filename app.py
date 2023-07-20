d872d8df922b0f543e8679e7fd45d70120297782
Jason Johnson <jasjoh@live.com>
2023-07-20 08:24:21 -0700

    final commit

--- app.py
+++ app.py
@@ -33,6 +33,27 @@ def view_questions(qnum):
 @app.post("/answer")
 def handle_answer():
     choice_picked = request.form["answer"]
-    print("value of answer in request.form:", choice_picked)
     RESPONSES.append(choice_picked)
-    # return redirect("/questions/0")
+    num_of_questions = len(survey.questions)
+    next_question_num = len(RESPONSES)
+    print("next question number:", next_question_num)
+
+    if (len(RESPONSES) < num_of_questions):
+        return redirect(f"/questions/{next_question_num}")
+    else:
+        return redirect("/thankyou")
+
+@app.get("/thankyou")
+def display_thankyou():
+    # RESPONSES looks like: ['Yes', 'No', 'Less than $10,000', 'Yes']
+    # we want to make an array of pairs where key is question, value is answer
+    # we want a list [(prompt1, answer1), (prompt2, answer2), (prompt3, answer3)]
+    num_of_questions = len(survey.questions)
+    range_questions = range(num_of_questions) # [0, 1, 2, 3] for 4 questions
+    prompt_to_answer = []
+    for index in range_questions:
+        value = RESPONSES[index]
+        key = survey.questions[index].prompt
+        prompt_to_answer.append((key, value))
+    print("prompt_to_answer:", prompt_to_answer)
+    return render_template("completion.html", prompt_to_answer=prompt_to_answer)
